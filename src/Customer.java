4cc0f27bcd06d02bc684ffad22afe58950b60191
Karan <tan22080@spengergasse.at>
2022-05-29 14:19:19 +0200

    Demo

---
+++ src/Customer.java
@@ -0,0 +1,26 @@
+
+public class Customer {
+	
+	private int customerid;
+	private String firstname;
+	private String lastname;
+	public int getCustomerid() {
+		return customerid;
+	}
+	public void setCustomerid(int customerid) {
+		this.customerid = customerid;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+}
